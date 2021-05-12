c4afbde0085cf8d973f6da2fe8e496c7972288d6
MINGKYME <admin@mingky.me>
2021-05-12 23:04:57 +0900

    no name 방지

--- index.js
+++ index.js
@@ -11,21 +11,27 @@ server.listen(5124, function() {
 var users = [];
 var handsUp = [];
 io.on('connect',function(socket){
-    socket.emit('handsup-list',JSON.stringify(handsUp));
-    users.push([socket,'']);
-    socket.broadcast.emit('user-count',users.length);
+    
     socket.on('init',function(name){
-        users.find((x) => x[0] == socket)[1] = name;
+        if(name != null && name != ""){
+            users.push([socket,name]);
+            socket.emit('handsup-list',JSON.stringify(handsUp));
+            socket.broadcast.emit('user-count',users.length);
+        }
     });
 
     socket.on('disconnect',function(){
-        users.splice(users.findIndex(x => x[0] == socket),1);
-        socket.broadcast.emit('user-count',users.length);
+        if(users.findIndex( (x)=> x[0] == socket) != -1){
+            users.splice(users.findIndex(x => x[0] == socket),1);
+            socket.broadcast.emit('user-count',users.length);
+        }
     });
     socket.on('handsup',function(){
-        let name = users.find((x)=>x[0].id == socket.id)[1];
-        if(handsUp.indexOf(name) == -1){
-            handsUp.push(name);
+        if(users.findIndex( (x)=> x[0] == socket) != -1){
+            let name = users.find((x)=>x[0].id == socket.id)[1];
+            if(handsUp.indexOf(name) == -1){
+                handsUp.push(name);
+            }
         }
         socket.broadcast.emit('handsup-list',JSON.stringify(handsUp)); 
     });
